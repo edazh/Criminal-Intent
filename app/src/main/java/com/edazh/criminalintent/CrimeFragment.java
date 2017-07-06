e317a56b2d9f1ffe38b7a49013ae4e330f70ccc9
edazh <edazh@qq.com>
2017-07-06 16:12:17 +0800

    完成详情界面

---
+++ app/src/main/java/com/edazh/criminalintent/CrimeFragment.java
@@ -0,0 +1,73 @@
+package com.edazh.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+/**
+ * Created by 12392 on 2017/7/6 0006.
+ * 罪行碎片
+ */
+
+public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+    private EditText mTitleField;
+    private Button mBtnDate;
+    private CheckBox mCbxSolved;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        //标题文本
+        mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        //日期按钮
+        mBtnDate = (Button) v.findViewById(R.id.crime_date);
+        mBtnDate.setText(mCrime.getDate().toString());
+        mBtnDate.setEnabled(false);
+
+        //是否解决checkbox
+        mCbxSolved = (CheckBox) v.findViewById(R.id.crime_solved);
+        mCbxSolved.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
+            }
+        });
+
+        return v;
+    }
+}
